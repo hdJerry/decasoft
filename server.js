fe9538a03a89b6927a8976f17dfd13b2d3ccfebe
hdjerry <damilolajerryhd@gmail.com>
2019-11-05 12:43:34 +0100

    First

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
 // console.log(path.join(__dirname, '/dist'));
-app.use(serveStatic(path.join(__dirname, '/dist')));
-const port = process.env.PORT || 8080;
+app.use(express.static(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 5000;
 app.listen(port);
 
 console.log("running here :" + port);
