95787fdad917c57555392ee86cf3867d335e11fe
hdjerry <damilolajerryhd@gmail.com>
2019-11-05 17:58:21 +0100

    First

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 app = express();
 // console.log(path.join(__dirname, '/dist'));
 app.use(function(req, res, next) {
-    res.setHeader("Content-Security-Policy", "default-src 'self' https://analytics.google.com https://dev-softnet.auth0.com https://manage-employees.herokuapp.com");
+    res.setHeader("Content-Security-Policy", "img-src 'self' ");
     return next();
 });
 app.use(express.static(path.join(__dirname, 'dist')));
